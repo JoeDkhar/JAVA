1285fe006f34d0a2b7b91600f3eeec6b0202424f
JoeDkhar <josaiah.dkhar.26@gmail.com>
2024-10-16 23:15:06 +0530

    rectified smart disposal system

--- Lab-Exercise-01/SmartDisposalManagementSystem.java
+++ Lab-Exercise-01/SmartDisposalManagementSystem.java
@@ -1,48 +1,34 @@
-public class SmartDisposalManagementSystem
-{
-    public static void main(String[] args)
-    {
-
-        WasteItem waste1 = new WasteItem("Plastic", 500, "Recycling", "North Region");
-        WasteItem waste2 = new WasteItem("Organic", 300, "Composting", "South Region");
-        WasteItem waste3 = new WasteItem("Hazardous", 150, "Incineration", "East Region");
-        WasteItem waste4 = new WasteItem("Metal", 200, "Recycling", "West Region");
-        WasteItem waste5 = new WasteItem("Glass", 100, "Recycling", "Central Region");
-
-
-        System.out.println("Total Waste Data:");
-        WasteItem.displayTotalWasteCollected();
-        WasteItem.displayCategoryTotals();
+public class SmartDisposalManagementSystem {
+    public static void main(String[] args) {
+        System.out.println("Initial Waste Items Creation:");
+        WasteItem plastic = new WasteItem("Plastic", 500, "Recycling", "North Region");
+        WasteItem organic = new WasteItem("Organic", 300, "Composting", "South Region");
+        WasteItem metal = new WasteItem("Metal", 250, "North Region");
+        WasteItem glass = new WasteItem("Glass", 150, "Recycling", "Central Region");
+        WasteItem paper = new WasteItem("Paper", 180, "Recycling", "East Region");
+        WasteItem hazardous = new WasteItem("Hazardous", 120, "Incineration", "West Region");
 
 
-        System.out.println("\nAll Waste Item Details:");
-        waste1.displayWasteDetails();
-        waste2.displayWasteDetails();
-        waste3.displayWasteDetails();
-        waste4.displayWasteDetails();
-        waste5.displayWasteDetails();
+        System.out.println("\nDisplaying Individual Waste Details:");
+        System.out.println("\nPlastic Waste Details:");
+        plastic.displayWasteDetails();
 
+        System.out.println("\nOrganic Waste Details:");
+        organic.displayWasteDetails();
 
-        System.out.println("\nUpdating waste item details:");
-        waste1.setQuantity(600);
-        waste1.setWasteType("Recyclable Plastic");
+        System.out.println("\nMetal Waste Details:");
+        metal.displayWasteDetails();
 
+        System.out.println("\nGlass Waste Details:");
+        glass.displayWasteDetails();
 
-        System.out.println("\nUpdated Waste Item 1 Details:");
-        waste1.displayWasteDetails();
+        System.out.println("\nPaper Waste Details:");
+        paper.displayWasteDetails();
 
+        System.out.println("\nHazardous Waste Details:");
+        hazardous.displayWasteDetails();
 
-        System.out.println("\nQuerying by region (South Region):");
-        if (waste2.getRegion().equals("South Region")) {
-            waste2.displayWasteDetails();
-        }
-
-
-        System.out.println("\nUpdating disposal method for waste3:");
-        waste3.updateDisposalMethod("Advanced Incineration");
-
-
-        System.out.println("\nFinal Waste Data:");
+        System.out.println("\nDisplaying Total Statistics:");
         WasteItem.displayTotalWasteCollected();
         WasteItem.displayCategoryTotals();
     }
--- Lab-Exercise-01/WasteItem.java
+++ Lab-Exercise-01/WasteItem.java
@@ -1,7 +1,7 @@
 class WasteItem {
     private String wasteType;
     private int quantity;
-    private StringBuffer disposalMethod;
+    private StringBuilder disposalMethod;
     private String region;
 
     private static int totalWasteCollected = 0;
@@ -9,21 +9,70 @@ class WasteItem {
     private static int compostingWaste = 0;
     private static int incinerationWaste = 0;
 
+    public WasteItem(String wasteType, int quantity, String region) {
+        this(wasteType, quantity, "Standard Disposal", region);
+    }
+
     public WasteItem(String wasteType, int quantity, String disposalMethod, String region) {
         this.wasteType = wasteType;
         this.quantity = quantity;
-        this.disposalMethod = new StringBuffer(disposalMethod);
+        this.disposalMethod = new StringBuilder(disposalMethod);
         this.region = region;
 
         totalWasteCollected += quantity;
         updateCategoryTotals(disposalMethod, quantity);
     }
 
-    public void displayWasteDetails() {
-        System.out.println("Waste Type: " + wasteType);
-        System.out.println("Quantity: " + quantity + " kg");
-        System.out.println("Disposal Method: " + disposalMethod);
-        System.out.println("Region: " + region);
+    public String getWasteType() {
+        return wasteType;
+    }
+
+    public void setWasteType(String wasteType) {
+        this.wasteType = wasteType;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        totalWasteCollected -= this.quantity;
+        totalWasteCollected += quantity;
+        this.quantity = quantity;
+    }
+
+    public String getDisposalMethod() {
+        return disposalMethod.toString();
+    }
+
+    public void setDisposalMethod(String disposalMethod) {
+        this.disposalMethod = new StringBuilder(disposalMethod);
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void updateDisposalMethod(String newMethod) {
+        this.disposalMethod = new StringBuilder(newMethod);
+    }
+
+    public void updateDisposalMethod(String methodPrefix, String methodSuffix) {
+        this.disposalMethod = new StringBuilder(methodPrefix + " " + methodSuffix);
+    }
+
+    public WasteItem addQuantity(int additionalQuantity) {
+        this.setQuantity(this.quantity + additionalQuantity);
+        return this;
+    }
+
+    public WasteItem updateRegion(String newRegion) {
+        this.setRegion(newRegion);
+        return this;
     }
 
     public static void displayTotalWasteCollected() {
@@ -34,6 +83,10 @@ class WasteItem {
         System.out.println("Recycling Waste: " + recyclingWaste + " kg");
         System.out.println("Composting Waste: " + compostingWaste + " kg");
         System.out.println("Incineration Waste: " + incinerationWaste + " kg");
+    }
+
+    public static int getTotalWasteCollected() {
+        return totalWasteCollected;
     }
 
     private static void updateCategoryTotals(String disposalMethod, int quantity) {
@@ -50,25 +103,11 @@ class WasteItem {
         }
     }
 
-    public void setQuantity(int quantity) {
-        totalWasteCollected -= this.quantity;
-        totalWasteCollected += quantity;
-        this.quantity = quantity;
-    }
-
-    public void setWasteType(String wasteType) {
-        this.wasteType = wasteType;
-    }
-
-    public String getRegion() {
-        return this.region;
-    }
-
-    public void updateDisposalMethod(String newMethod) {
-        this.disposalMethod.replace(0, this.disposalMethod.length(), newMethod);
-    }
-
-    public void updateDisposalMethod(String methodPrefix, String methodSuffix) {
-        this.disposalMethod.replace(0, this.disposalMethod.length(), methodPrefix + " " + methodSuffix);
+    public void displayWasteDetails() {
+        System.out.println("Waste Type: " + wasteType);
+        System.out.println("Quantity: " + quantity + " kg");
+        System.out.println("Disposal Method: " + disposalMethod);
+        System.out.println("Region: " + region);
     }
 }
+
